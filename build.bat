7894788ba9fab631f5a83879483b8d0f180e90d0
SebMay99 <tianmayorga2008@gmail.com>
2026-01-14 02:43:16 -0600

    Updated build.bat to reflect v0.5-beta.1401

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 streamlit-desktop-app build app.py ^
- --name HPE_Financial_App_v0.4-beta.1301 ^
+ --name HPE_Financial_App_v0.5-beta.1401 ^
  --icon .\assets\HPE_icon.png ^
  --pyinstaller-options ^
  --add-data "functions\processing.py;functions" ^
