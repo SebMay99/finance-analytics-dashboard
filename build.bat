ecbd289e9daf57c3a5f0f4e82a5f8549f0630053
Sebastián <tianmayorga2008@gmail.com>
2026-03-01 12:19:37 -0600

    Fixed typo in app name

--- build.bat
+++ build.bat
@@ -9,7 +9,7 @@ if exist *.spec del /q *.spec >nul 2>&1
 
 echo Building optimized executable...
 streamlit-desktop-app build app.py ^
- --name GreenLake Pulse ^
+ --name GreenLake_Pulse ^
  --icon .\assets\HPE_icon.png ^
  --pyinstaller-options ^
  --add-data "functions;functions" ^
